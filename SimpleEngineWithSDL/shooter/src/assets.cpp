be5452c2d64a667c205e5e65cecb1689ff7ab427
Simon Lambin <slambin@artfx.fr>
2020-10-12 23:43:38 +0200

    feat(class Assets): Create the Assets class

---
+++ SimpleEngineWithSDL/shooter/src/assets.cpp
@@ -0,0 +1,38 @@
+#include "assets.h"
+#include "log.h"
+#include <sstream>
+
+std::map<std::string, Texture> Assets::textures;
+
+Texture Assets::loadTexture(Renderer& renderer, const std::string fileName, const std::string& name)
+{
+	textures[name] = loadTextureFromFile(renderer, fileName.c_str());
+	return textures[name];
+}
+
+Texture& Assets::getTexture(const std::string& name)
+{
+	if (textures.find(name) == end(textures))
+	{
+		std::ostringstream loadError;
+		loadError << "Texture" << name << "does not exist in asset manager.";
+		Log::error(LogCategory::Application, loadError.str());
+	}
+	return textures[name];
+}
+
+void Assets::clear()
+{
+	for (auto iter : textures)
+	{
+		iter.second.unload();
+	}
+	textures.clear();
+}
+
+Texture Assets::loadTextureFromFile(Renderer& renderer, const std::string& fileName)
+{
+	Texture texture;
+	texture.load(renderer, fileName);
+	return texture;
+}
---
+++ SimpleEngineWithSDL/shooter/src/assets.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "texture.h"
+#include <string>
+#include <map>
+
+// A static singleton Asset class that hosts several
+// functions to load resources.
+class Assets
+{
+public:
+	static std::map < std::string, Texture> textures;
+
+	static Texture loadTexture(Renderer& renderer, const std::string fileName, const std::string& name);
+	static Texture& getTexture(const std::string& name);
+	static void clear();
+
+private:
+	Assets() {}
+
+	static Texture loadTextureFromFile(Renderer& renderer, const std::string& fileName);
+};
