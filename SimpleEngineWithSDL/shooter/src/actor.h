3116312d6604e6729d1f047263c867ac979c6e5f
Simon Lambin <slambin@artfx.fr>
2020-10-12 22:24:36 +0200

    feat(class Actor): Create the Actor class

---
+++ SimpleEngineWithSDL/shooter/src/actor.h
@@ -0,0 +1,45 @@
+#pragma once
+#include <vector>
+#include "vector2.h"
+
+class Game;
+class Component;
+
+class Actor
+{
+public:
+	enum class ActorState
+	{
+		Active, Paused, Dead
+	};
+
+	Actor();
+	virtual ~Actor();
+	Actor(const Actor&) = delete;
+	Actor& operator=(const Actor&) = delete;
+
+	Game& getFrame() const { return game; }
+	const ActorState getState() const { return state; }
+	const Vector2 getPosition() const { return position; }
+	const float getScale() const { return scale; }
+	const float getRotation() const { return rotation; }
+
+	void setPosition(Vector2 positionP);
+	void setScale(float scaleP);
+	void setRotation(float rotationP);
+
+	void update(float dt);
+	void updateComponent(float dt);
+	virtual void updateActor(float dt);
+	void addComponent(Component* component);
+	void removeComponent(Component* component);
+
+private:
+	Game& game;
+	ActorState state;
+	Vector2 position;
+	float scale;
+	float rotation;
+
+	std::vector<Component*> components;
+};
