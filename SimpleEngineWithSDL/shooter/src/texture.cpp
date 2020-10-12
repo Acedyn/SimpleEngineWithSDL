38b7c755ff837fd5cf9264bf7b22c75539905e17
Simon Lambin <slambin@artfx.fr>
2020-10-12 23:22:12 +0200

    feat(class Texture): Create the Texture class

---
+++ SimpleEngineWithSDL/shooter/src/texture.cpp
@@ -0,0 +1,49 @@
+#include "texture.h"
+#include "log.h"
+#include <SDL_image.h>
+
+Texture::Texture() :
+	fileName(""),
+	width(0),
+	height(0),
+	SDLTexture(nullptr) {}
+
+Texture::~Texture() {}
+
+void Texture::unload()
+{
+	if (SDLTexture)
+	{
+		SDL_DestroyTexture(SDLTexture);
+	}
+}
+
+bool Texture::load(Renderer& rendererP, const std::string& fileNameP)
+{
+	fileName = fileNameP;
+	SDL_Surface* surf = IMG_Load(fileName.c_str());
+	if (!surf)
+	{
+		Log::error(LogCategory::Application, "Failed to load texture file" + fileName);
+		return false;
+	}
+	width = surf->w;
+	height = surf->h;
+
+	SDLTexture = SDL_CreateTextureFromSurface(rendererP.toSDLRenderer(), surf);
+	SDL_FreeSurface(surf);
+	if (!SDLTexture)
+	{
+		Log::error(LogCategory::Render, "Failed to convert to texture for " + fileName);
+		return false;
+	}
+	Log::info("Loaded texture" + fileName);
+
+	return true;
+}
+
+void Texture::updateInfo(int& widthOut, int& heightOut)
+{
+	widthOut = width;
+	heightOut = height;
+}
---
+++ SimpleEngineWithSDL/shooter/src/texture.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "renderer.h"
+#include <string>
+
+class Texture
+{
+public:
+	Texture();
+	~Texture();
+
+	void unload();
+	bool load(Renderer& rendererP, const std::string& fileNameP);
+	inline void updateInfo(int& widthOut, int& heightOut);
+
+	inline int getWidth() const { return width; }
+	inline int getHeight() const { return height; }
+
+private:
+	std::string fileName;
+	SDL_Texture* SDLTexture;
+	int width;
+	int height;
+};
